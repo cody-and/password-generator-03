e16cdd807d21732d48ab70ad881dc3f3de7f2101
cody-and <codya.0807@icloud.com>
2023-07-03 23:21:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// Assignment Code
+
+var howmanyCharacters;
+
+var generateBtn = document.querySelector("#generate");
+
+
+function generatePassword(){
+
+
+
+
+
+
+  return
+}
+
+function askHowManyCharacters(){
+
+
+
+  askAboutSpecialCharacters()
+}
+
+
+function askAboutSpecialCharacters (){
+
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
